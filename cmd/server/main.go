983f96a67695e7434de3c2c301485d3e0bc658ac
mohammed-ayoub-dz <mohammedayoub852@proton.me>
2026-05-22 21:48:51 +0100

    fix bug

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -44,7 +44,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, https://endline-beta.vercel.app",
+		AllowOrigins:     "https://endline-beta.vercel.app",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
